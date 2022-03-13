6c2a0fa33d82e7915b3ae58c74b16be4829bbea6
Aleksei Kotvitskyi <kot.aleksei43@gmail.com>
2022-03-13 19:01:36 +0200

    feat: updates

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require("express");
-// const path = require("path");
+var express = require("express");
+var app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-express()
-  .get("/", (req, res) => res.render("index"))
+// app()
+//   .get("/", (req, res) => res.render("index"))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app
+  .get(
+    "/now",
+    function (req, res, next) {
+      req.time = new Date().toString();
+      next();
+    },
+    function (req, res) {
+      res.send({ time: req.time });
+    }
+  )
+  .get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
